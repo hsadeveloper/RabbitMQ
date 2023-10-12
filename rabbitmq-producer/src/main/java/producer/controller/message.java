054547c64377d7031e6933f8faeb261aafbda520
Hsabonchi <developersabon@gmail.com>
2023-10-11 21:52:56 -0500

    Step 4: Create controller

---
+++ rabbitmq-producer/src/main/java/producer/controller/message.java
@@ -0,0 +1,23 @@
+package producer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import producer.service.ProduceMessageService;
+
+
+public class message {
+	@RestController
+	public class ProduceMessageController {
+
+	    @Autowired
+	    ProduceMessageService produceMessageService;
+
+	    @GetMapping("/produce")
+	    public String produceMessage() {
+	        return produceMessageService.produceMessage("Hello nmmmm");
+	    }
+	    
+    }
+}
