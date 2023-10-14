fa8bce2a4e887171191f7654dbd7194742ca4dc9
Hsabonchi <developersabon@gmail.com>
2023-10-13 23:30:11 -0500

    Step 2: Create configuration

---
+++ rabbitmq-consumer/src/main/java/consumer/configuration/ConsumerConfig.java
@@ -0,0 +1,34 @@
+package consumer.configuration;
+
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import consumer.service.ConsumeMessageService;
+
+public class ConsumerConfig {
+	
+
+	@Configuration
+	public class ConfigureRabbitMq {
+
+	    public static final String QUEUE_NAME = "myQueue";
+
+	    @Bean
+	    SimpleMessageListenerContainer simpleMessageListenerContainer(ConnectionFactory connectionFactory, MessageListenerAdapter messageListenerAdapter) {
+	        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+	        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+	        simpleMessageListenerContainer.setQueueNames(QUEUE_NAME);
+	        simpleMessageListenerContainer.setMessageListener(messageListenerAdapter);
+	        return simpleMessageListenerContainer;
+	    }
+
+	    @Bean
+	    MessageListenerAdapter messageListenerAdapter(ConsumeMessageService consumeMessageService) {
+	        return new MessageListenerAdapter(consumeMessageService, "consumeMessage");
+	    }
+}
+}
